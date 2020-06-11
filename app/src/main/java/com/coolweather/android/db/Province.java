ac41a8c2c32e21ba62ac22f74ddc6fee12b88260
hzd9970 <hzd9970@arcsoft.com.cn>
2020-06-11 15:58:49 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,35 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Province extends LitePalSupport {
+    private int id;
+
+    private String proviceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProviceName() {
+        return proviceName;
+    }
+
+    public void setProviceName(String proviceName) {
+        this.proviceName = proviceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
